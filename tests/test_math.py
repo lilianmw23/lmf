b9da6067737a0727717cb7b06d456f53833b6aef
lilianmw23 <lilianmw23@gmail.com>
2021-12-01 20:35:02 -0800

    updated parameter for mulltipication function

--- tests/test_math.py
+++ tests/test_math.py
@@ -6,7 +6,7 @@ def test_addition():
 def test_subtraction():
     assert 1 - 1 == 0
 
-@pytest.mark.parametrize("a,b,expected", [(0, 5, 0), (1, 5, 5), (2, 5, 10), (-3, 5, -15), (-4, -5, 20)])
+@pytest.mark.parametrize("a,b,expected", [(0, 5, 0), (1, 5, 5), (2, 5, 10), (-3, 5, -15), (-5, -5, 25)])
 def test_multipication(a, b, expected):
     assert a * b == expected
 
