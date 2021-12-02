1818df0054d6255edd91eb515b0a05c566c90b18
Lilian N <lilianmw23@gmail.com>
2021-12-01 19:59:59 -0800

    basic calculations

---
+++ tests/test_math.py
@@ -0,0 +1,15 @@
+import pytest
+
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 1 - 1 == 0
+
+@pytest.mark.parametrize("a,b,expected", [(0, 5, 0), (1, 5, 5), (2, 5, 10), (-3, 5, -15), (-4, -5, 20)])
+def test_multipication(a, b, expected):
+    assert a * b == expected
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
